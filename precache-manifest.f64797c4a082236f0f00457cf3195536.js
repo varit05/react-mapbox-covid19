2dde01a96846f6a3dba19ebdbb363faf069ed7af
varit05 <varitpatel@gmail.com>
2020-04-11 13:01:52 +0530

    Updates

---
+++ precache-manifest.f64797c4a082236f0f00457cf3195536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebe626f401ba21274c94f957935e28e",
+    "url": "/react-mapbox-covid19/index.html"
+  },
+  {
+    "revision": "3ac8ad15687f73576288",
+    "url": "/react-mapbox-covid19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3fc5970aa18f7dc8c782",
+    "url": "/react-mapbox-covid19/static/js/2.a44be610.chunk.js"
+  },
+  {
+    "revision": "b4610c35fd5e6efa7493e375f2f8ce97",
+    "url": "/react-mapbox-covid19/static/js/2.a44be610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac8ad15687f73576288",
+    "url": "/react-mapbox-covid19/static/js/main.e6d8205e.chunk.js"
+  },
+  {
+    "revision": "79a6843002974e3fa170",
+    "url": "/react-mapbox-covid19/static/js/runtime-main.d7b36054.js"
+  }
+]);
